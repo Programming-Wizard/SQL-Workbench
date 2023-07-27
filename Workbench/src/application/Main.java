c1dac79e1a17bc6e6a815e654e9e539cf9665c59
Programming-Wizard <varun.02.11.04@gmail.com>
2023-07-28 01:20:41 +0530

    fixed the issue where the user query was not being executed, also fixed the classnotfound exception, now the program accepts the queries and his executing

--- Workbench/src/application/Main.java
+++ Workbench/src/application/Main.java
@@ -11,10 +11,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/WorkBenchBuild.fxml"));
+//			Parent root = FXMLLoader.load(getClass().getResource("/WorkBenchBuild.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("The Basic SQL Queries Executor");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
