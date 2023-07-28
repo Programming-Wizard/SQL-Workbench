314c12209ff3ac175f450c4430a7d248f93c147c
Programming-Wizard <varun.02.11.04@gmail.com>
2023-07-28 21:40:44 +0530

    added logic for fading out of error label and image, and fixed when it should be visible

--- Workbench/src/application/ControllerForLogin.java
+++ Workbench/src/application/ControllerForLogin.java
@@ -6,14 +6,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -28,6 +31,10 @@ public class ControllerForLogin implements Initializable {
 	private TextField username;
 	@FXML
 	private PasswordField password;
+	@FXML
+	private Label errorLabel;
+	@FXML
+	private ImageView errorImage;
 	private Media media;
 	private MediaPlayer mediaplayer;
 	// public boolean loginRequested = false;
@@ -35,9 +42,16 @@ public class ControllerForLogin implements Initializable {
 	private static String passwordData;
 	private Timeline hoverAnimation;
 	private Timeline notHoveredAnimation;
+	private String errorMessage1 = "\"Invalid password or unable to connect to the database.\"\n"
+			+ "\r\n"
+			+ "Enable SQL service or "
+			+ "Enter correct password.";
+	private String errorMessage2 = "Password is a required field";
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		errorImage.setVisible(false);
+		errorLabel.setVisible(false);
 		media = new Media(getClass().getResource("/background2.mp4").toExternalForm());
 		mediaplayer = new MediaPlayer(media);
 		background.setMediaPlayer(mediaplayer);
@@ -84,6 +98,8 @@ public class ControllerForLogin implements Initializable {
 
 //	establishing and checking if there is no problem with making a connection with the server or the user entered password
 	public boolean loginRequest() {
+		errorImage.setVisible(false);
+		errorLabel.setVisible(false);
 		this.usernameData = username.getText();
 		this.passwordData = password.getText();
 		//		this.passwordData = hashPassword(password.getText());
@@ -101,16 +117,53 @@ public class ControllerForLogin implements Initializable {
 				e.printStackTrace();
 			} catch (SQLException e) 
 			{
-				System.out.println("Invalid password or database connection error.Either try turning the service on for your SQL service or Enter correct password");
+//				System.out.println("");
+				displayingError(errorMessage1);
 				return false;
 			}
 			}
 		else
 		{
-			System.out.println("The password is required");
+//			System.out.println("The password is required");
+			displayingError(errorMessage2);
 		}
 		return false;
 	}
+
+	
+	public void displayingError(String Message)
+	{
+		errorLabel.setVisible(true);
+		errorImage.setVisible(true);
+		errorLabel.setText(Message);
+		errorLabel.setOpacity(1);
+		errorImage.setOpacity(1);
+		
+		Duration showDuration = Duration.seconds(3);
+	    Timeline showDurationTimeline = new Timeline(new KeyFrame(showDuration, event -> {
+	        FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), errorLabel);
+	        fadeOut.setToValue(0);
+	        fadeOut.setOnFinished(e -> {
+	            errorLabel.setVisible(false);
+	        });
+	        fadeOut.play();
+	    }));
+	    showDurationTimeline.stop();
+	    showDurationTimeline.play();
+	    Duration showDuration2 = Duration.seconds(3);
+	    Timeline showDurationTimeline2 = new Timeline(new KeyFrame(showDuration2, event -> {
+	    	FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), errorImage);
+	    	fadeOut.setToValue(0);
+	    	fadeOut.setOnFinished(e -> {
+	    		errorLabel.setVisible(false);
+	    	});
+	    	fadeOut.play();
+	    }));
+	    showDurationTimeline2.stop();
+	    showDurationTimeline2.play();
+		
+	}
+	
 //	getting the user defined username and password using getters
 	public static String getUsername() 
 	{
