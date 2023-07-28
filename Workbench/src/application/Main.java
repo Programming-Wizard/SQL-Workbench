784135a4b01c3fae15bac98f6f74bef1c0af79ed
Programming-Wizard <varun.02.11.04@gmail.com>
2023-07-28 18:05:57 +0530

    handled scene changes when login is requested from the user

--- Workbench/src/application/Main.java
+++ Workbench/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,13 +13,39 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-//			Parent root = FXMLLoader.load(getClass().getResource("/WorkBenchBuild.fxml"));
-			Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+			FXMLLoader LoginScreen= new FXMLLoader(getClass().getResource("/Login.fxml"));
+			Parent root = LoginScreen.load();
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("The Basic SQL Queries Executor");
+			
+			ControllerForLogin loginController = LoginScreen.getController();
+			loginController.loginBtn.setOnMouseClicked(event -> {
+				if( loginController.loginRequest() && ControllerForLogin.getPassword().length() > 0)
+				{
+						FXMLLoader workBenchScreen= new FXMLLoader(getClass().getResource("/WorkBenchBuild.fxml"));
+						try {
+							Parent newRoot = workBenchScreen.load();
+							Scene workBench = new Scene(newRoot);
+							primaryStage.setScene(workBench);
+							
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+				}
+				else
+				{
+					System.out.println("Enter the password");
+				}
+				});
+			
+				
+			
+			primaryStage.setResizable(false);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
