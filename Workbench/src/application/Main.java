9e5dca5ca01e2f00434ce4a6a064aae4bf1bdd3a
Programming-Wizard <varun.02.11.04@gmail.com>
2023-07-28 19:05:22 +0530

    removed the condition where it checked the password length and implemented it in the login screen itself

--- Workbench/src/application/Main.java
+++ Workbench/src/application/Main.java
@@ -1,6 +1,4 @@
 package application;
-	
-import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,49 +6,43 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader LoginScreen= new FXMLLoader(getClass().getResource("/Login.fxml"));
+			FXMLLoader LoginScreen = new FXMLLoader(getClass().getResource("/Login.fxml"));
 			Parent root = LoginScreen.load();
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("The Basic SQL Queries Executor");
-			
+
 			ControllerForLogin loginController = LoginScreen.getController();
 			loginController.loginBtn.setOnMouseClicked(event -> {
-				if( loginController.loginRequest() && ControllerForLogin.getPassword().length() > 0)
+				if (loginController.loginRequest()) 
 				{
-						FXMLLoader workBenchScreen= new FXMLLoader(getClass().getResource("/WorkBenchBuild.fxml"));
-						try {
-							Parent newRoot = workBenchScreen.load();
-							Scene workBench = new Scene(newRoot);
-							primaryStage.setScene(workBench);
-							
-						}
-						catch (Exception e) {
-							e.printStackTrace();
-						}
-				}
-				else
-				{
-					System.out.println("Enter the password");
-				}
-				});
-			
-				
-			
+					FXMLLoader workBenchScreen = new FXMLLoader(getClass().getResource("/WorkBenchBuild.fxml"));
+					try {
+						Parent newRoot = workBenchScreen.load();
+						Scene workBench = new Scene(newRoot);
+						primaryStage.setScene(workBench);
+
+					} catch (Exception e) 
+					{
+						e.printStackTrace();
+					}
+				} 
+			});
+
 			primaryStage.setResizable(false);
 			primaryStage.show();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
