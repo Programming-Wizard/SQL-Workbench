4b3892d22e353f6ed918b2b9eb056476feceff01
Programming-Wizard <varun.02.11.04@gmail.com>
2023-07-28 19:06:31 +0530

    minor indentations adjustment

--- Workbench/src/application/WorkBenchController.java
+++ Workbench/src/application/WorkBenchController.java
@@ -1,4 +1,5 @@
 package application;
+
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -22,61 +23,59 @@ public class WorkBenchController implements Initializable {
 	private TextArea WorkBench;
 	@FXML
 	private ScrollPane scrollPane;
-	
+
 	private int lineCount = 1;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-        Linecontainer.getChildren().add(new Text(" 1"));
+		Linecontainer.getChildren().add(new Text(" 1"));
 
-        WorkBench.setOnKeyPressed(event -> 
-        {
-        	if (event.isControlDown() && (event.getCode() == KeyCode.ENTER)) {
-        		
-        		int CursorPosition = WorkBench.getCaretPosition();
-        		System.out.println(CursorPosition);
-        		
-        		WorkBench.replaceSelection("\n");
-        		
-        		WorkBench.positionCaret(CursorPosition + 1);
-        		
-                lineCount++;
-                Linecontainer.getChildren().add(new Text(" "+String.valueOf(lineCount)));
-        	}
-        	else if (event.getCode() == KeyCode.ENTER) 
-        	{
-                lineCount++;
-                Linecontainer.getChildren().add(new Text(" " + String.valueOf(lineCount)));
-            }
-        	
-        });
-        
-    
-    }
-	
-	
-	public void workbenchdata()
+		WorkBench.setOnKeyPressed(event -> {
+			if (event.isControlDown() && (event.getCode() == KeyCode.ENTER)) {
+
+				int CursorPosition = WorkBench.getCaretPosition();
+				System.out.println(CursorPosition);
+
+				WorkBench.replaceSelection("\n");
+
+				WorkBench.positionCaret(CursorPosition + 1);
+
+				lineCount++;
+				Linecontainer.getChildren().add(new Text(" " + String.valueOf(lineCount)));
+			} 
+			else if (event.getCode() == KeyCode.ENTER) 
+			{
+				lineCount++;
+				Linecontainer.getChildren().add(new Text(" " + String.valueOf(lineCount)));
+			}
+
+		});
+
+	}
+
+	public void workbenchdata() 
 	{
 		String Data = WorkBench.getText();
 		String username = ControllerForLogin.getUsername();
 		String password = ControllerForLogin.getPassword();
 		String url = "jdbc:mysql://localhost:3306/my_database";
-		
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
+
 			Connection con = DriverManager.getConnection(url, username, password);
 			Statement stmt = con.createStatement();
 			String sqlQuery = Data;
-//			try {
-//			int resultSet = stmt.executeUpdate(sqlQuery);
+			//			try {
+			//			int resultSet = stmt.executeUpdate(sqlQuery);
 			ResultSet resultSet = stmt.executeQuery(sqlQuery);
 			System.out.println("query executed succesfully");
-//			}
-//			catch (Exception e) {
-//				System.out.println(e);
-//			}
-			
-			while(resultSet.next())
+			//			}
+			//			catch (Exception e) {
+			//				System.out.println(e);
+			//			}
+
+			while (resultSet.next()) 
 			{
 				int id = resultSet.getInt("id");
 				String name = resultSet.getString("name");
@@ -86,12 +85,13 @@ public class WorkBenchController implements Initializable {
 			}
 			stmt.close();
 			con.close();
-		} catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) 
+		{
 			e.printStackTrace();
-		} catch (SQLException e) {
+		} catch (SQLException e) 
+		{
 			e.printStackTrace();
 		}
 	}
 
 }
-
